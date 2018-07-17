a041d46b05b26ac42540e135068acb2a2cc97eeb
chi <ciyalnin@gmail.com>
2018-07-17 22:26:54 +0800

    [update] set target auto

--- createDataForPath.cpp
+++ createDataForPath.cpp
@@ -8,24 +8,34 @@ using namespace std;
 int main()
 {
 	Mat image;
-	String filePlace = "E:/testvs/pdata/0713/hull128/";
+	String filePlace = "E:/testvs/pdata/0717/hull128/";
 	String srcfileType = ".jpg";
 	String srcfilePlace = "E:/testvs/pdata/0713/hull/";
-	int fIndex = 452;
+	int fIndex = 453;
 	int ROISize = 128;
-	int globalIndex = 157;
+	float ROIArea = ROISize * ROISize;
+	float cloudRateThreshold = 0.1;
+	float cloudDiffThreshold = 0.05;
+	int fullBlack = 0;
+	int fullWhite = 0;
+	int globalIndex = 1;
 	int x, y, col, row, key;
-
+	int leftCloud = 0, rightCloud = 0;
 	Mat subImg;
+	time_t ctt = time(0);
+	cout << asctime(localtime(&ctt)) << std::endl;
 	while (true)
 	{
-		fIndex++;
 		
+
 		String imgName = to_string(fIndex);
-		image = cv::imread(srcfilePlace + imgName + srcfileType);
+		image = cv::imread(srcfilePlace + imgName + srcfileType, CV_LOAD_IMAGE_GRAYSCALE);
 		if (!image.data) // Check for invalid input
 		{
 			cout << "Could not open or find the image" << std::endl;
+			ctt = time(0);
+			cout << asctime(localtime(&ctt)) << std::endl;
+			system("pause");
 			return -1;
 		}
 		x = 0;
@@ -46,17 +56,70 @@ int main()
 				if (j == col - 1)
 					x = image.cols - ROISize;
 				subImg = image(Rect(x, y, ROISize, ROISize));
-				namedWindow(imgName + "_" + to_string(i) + to_string(j));
-				imshow(imgName + "_" + to_string(i) + to_string(j), subImg);
-				cout << "fIndex " << fIndex <<" "<< i<<" "<<j<<" key = ";
-				key = waitKey(0);
-				cout << key << endl;
+				leftCloud = 0;
+				rightCloud = 0;
+
+				for (int k = 0; k < subImg.rows; k++)
+				{
+					uchar *ptr1 = subImg.ptr<uchar>(k);
+					for (int z = 0; z < subImg.cols; z++)
+					{
+						if ((int)ptr1[z] == 255)
+						{
+							if (z < (subImg.cols / 2))
+							{
+								leftCloud++;
+							}
+							else
+							{
+								rightCloud++;
+							}
+						}
+					}
+				}
+
+				if (leftCloud == 0 && rightCloud == 0)
+				{
+					key = 0;
+					fullBlack++;
+					if (fullBlack == 1000) cout << "FB=> "<< globalIndex;
+					if (fullBlack >= 1000) continue;
+				}
+				else if (leftCloud == (int)(ROIArea / 2) && rightCloud == (int)(ROIArea / 2)) {
+					key = 0;
+					fullWhite++;
+					if (fullWhite == 1000) cout << "FW=> " << globalIndex;
+					if (fullWhite >= 1000) continue;
+				}
+				else if ((abs(leftCloud - rightCloud) / (ROIArea) < cloudDiffThreshold))
+				{
+					key = 0;
+				}
+				else if (leftCloud > rightCloud && leftCloud > cloudRateThreshold)
+				{
+					key = 2;
+				}
+				else if (leftCloud <= rightCloud)
+				{
+					key = 1;
+				}
+
+				//namedWindow(imgName + "_" + to_string(i) + to_string(j));
+				//imshow(imgName + "_" + to_string(i) + to_string(j), subImg);
+				//key = waitKey(0);
+				//cout << globalIndex << "  fIndex " << fIndex << " " << i << " " << j << " key = " << key << endl;
+				float temp = (abs(leftCloud - rightCloud) / (ROIArea));
+				//cout << "    L = " << leftCloud << " R = " << rightCloud << "  diff= " << temp << endl;
 				// key num 1(left) = 49, num 2(top) = 50, num 3(right) = 51;
-				imwrite(filePlace + to_string(globalIndex) + "_" + to_string(key - 50) + ".jpg", subImg);
+				imwrite(filePlace + to_string(globalIndex) + "_" + to_string(key) + ".jpg", subImg);
 				globalIndex++;
+
 			}
 		}
+		fIndex++;
 	}
-
+	ctt = time(0);
+	cout << asctime(localtime(&ctt)) << std::endl;
+	system("pause");
 	return 0;
 }
