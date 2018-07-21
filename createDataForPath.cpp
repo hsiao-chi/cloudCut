ba3a2639cf18cf432aede225f18d943007a72c3f
chi <ciyalnin@gmail.com>
2018-07-21 22:41:12 +0800

    [update] ROISize_width and height

--- createDataForPath.cpp
+++ createDataForPath.cpp
@@ -8,14 +8,16 @@ using namespace std;
 int main()
 {
 	Mat image;
-	String filePlace = "E:/testvs/pdata/0717/hull128/";
+	String filePlace = "E:/testvs/pdata/0721/hull256x128/";
 	String srcfileType = ".jpg";
 	String srcfilePlace = "E:/testvs/pdata/0713/hull/";
 	int fIndex = 453;
-	int ROISize = 128;
-	float ROIArea = ROISize * ROISize;
+	int ROISize_width = 256;
+	int ROISize_height = 128;
+	float ROIArea = ROISize_width * ROISize_height;
 	float cloudRateThreshold = 0.1;
 	float cloudDiffThreshold = 0.05;
+	float noCloudRateThreshold = 0.1;
 	int fullBlack = 0;
 	int fullWhite = 0;
 	int globalIndex = 1;
@@ -26,7 +28,6 @@ int main()
 	cout << asctime(localtime(&ctt)) << std::endl;
 	while (true)
 	{
-		
 
 		String imgName = to_string(fIndex);
 		image = cv::imread(srcfilePlace + imgName + srcfileType, CV_LOAD_IMAGE_GRAYSCALE);
@@ -40,22 +41,22 @@ int main()
 		}
 		x = 0;
 		y = 0;
-		col = image.cols / ROISize;
-		col = (image.cols % ROISize) > (ROISize / 2) ? (col + 1) : col;
-		row = image.rows / ROISize;
-		row = (image.rows % ROISize) > (ROISize / 2) ? (row + 1) : row;
+		col = image.cols / ROISize_width;
+		col = (image.cols % ROISize_width) > (ROISize_width / 2) ? (col + 1) : col;
+		row = image.rows / ROISize_height;
+		row = (image.rows % ROISize_height) > (ROISize_height / 2) ? (row + 1) : row;
 
 		for (int i = 0; i < row; i++)
 		{
-			y = i * ROISize;
+			y = i * ROISize_height;
 			if (i == row - 1)
-				y = image.rows - ROISize;
+				y = image.rows - ROISize_height;
 			for (int j = 0; j < col; j++)
 			{
-				x = j * ROISize;
+				x = j * ROISize_width;
 				if (j == col - 1)
-					x = image.cols - ROISize;
-				subImg = image(Rect(x, y, ROISize, ROISize));
+					x = image.cols - ROISize_width;
+				subImg = image(Rect(x, y, ROISize_width, ROISize_height));
 				leftCloud = 0;
 				rightCloud = 0;
 
@@ -82,28 +83,38 @@ int main()
 				{
 					key = 0;
 					fullBlack++;
-					if (fullBlack == 1000) cout << "FB=> "<< globalIndex;
-					if (fullBlack >= 1000) continue;
+					if (fullBlack == 1000)
+						cout << "FB=> " << globalIndex;
+					if (fullBlack >= 1000)
+						continue;
 				}
-				else if (leftCloud == (int)(ROIArea / 2) && rightCloud == (int)(ROIArea / 2)) {
+				else if (leftCloud == (int)(ROIArea / 2) && rightCloud == (int)(ROIArea / 2))
+				{
 					key = 0;
 					fullWhite++;
-					if (fullWhite == 1000) cout << "FW=> " << globalIndex;
-					if (fullWhite >= 1000) continue;
+					if (fullWhite == 1000)
+						cout << "FW=> " << globalIndex;
+					if (fullWhite >= 1000)
+						continue;
 				}
 				else if ((abs(leftCloud - rightCloud) / (ROIArea) < cloudDiffThreshold))
 				{
 					key = 0;
 				}
-				else if (leftCloud > rightCloud && leftCloud > cloudRateThreshold)
+				else if ((leftCloud + rightCloud) / ROIArea > noCloudRateThreshold)
 				{
-					key = 2;
+					if (leftCloud > rightCloud && leftCloud / (ROIArea / 2) > cloudRateThreshold)
+					{
+						key = 2;
+					}
+					else if (leftCloud <= rightCloud && rightCloud / (ROIArea / 2) > cloudRateThreshold)
+					{
+						key = 1;
+					}
 				}
-				else if (leftCloud <= rightCloud && rightCloud > cloudRateThreshold)
+
+				else
 				{
-					key = 1;
-				}
-				else{
 					key = 0;
 				}
 
@@ -116,7 +127,6 @@ int main()
 				// key num 1(left) = 49, num 2(top) = 50, num 3(right) = 51;
 				imwrite(filePlace + to_string(globalIndex) + "_" + to_string(key) + ".jpg", subImg);
 				globalIndex++;
-
 			}
 		}
 		fIndex++;
