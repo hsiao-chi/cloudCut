396c16cf922949e0550ac51437a3dcdf07bf715f
chi <ciyalnin@gmail.com>
2018-07-28 23:29:37 +0800

    [change] hog training ok

--- cloudClassify.cpp
+++ cloudClassify.cpp
@@ -9,21 +9,27 @@
 #include "opencv2/imgcodecs.hpp"
 #include <opencv2/highgui.hpp>
 #include <opencv2/ml.hpp>
+#include <opencv2/objdetect.hpp>
 
 using namespace cv;
 using namespace std;
 using namespace ml;
 
-String record = "C:\\Users\\yozorasa\\Documents\\GraduateSchool\\space\\lbpRename\\";
-String loadLocationC = "C:\\Users\\yozorasa\\Documents\\GraduateSchool\\space\\lbpRename\\cloud\\";
-String loadLocationO = "C:\\Users\\yozorasa\\Documents\\GraduateSchool\\space\\lbpRename\\other\\";
+String record = "E:\\testvs\\pdata\\srcImg\\lbpRename\\";
+String loadLocationC = "E:\\testvs\\pdata\\srcImg\\lbpRename\\cloud\\";
+String loadLocationO = "E:\\testvs\\pdata\\srcImg\\lbpRename\\other\\";
 String fileType = ".jpg";
 
-int cloudAmount = 5770 / 2;
-int otherAmount = 1780 / 2;
+int cloudAmount = 498 ;
+int otherAmount = 251 ;
 float histTemp[256] = { 0 };
-float histogramCal[5770 / 2 + 1780 / 2][256] = { 0 };
-int tag[5770 / 2 + 1780 / 2] = { 0 };
+float histogramCal[498 + 251][256] = { 0 };
+int tag[498 + 251] = { 0 };
+
+
+HOGDescriptor *hog = new HOGDescriptor(Size(64,64), Size(16,16), Size(8,8), Size(8,8), 9, 1 );
+
+vector< Mat >  hogDatas;
 
 int histogram(Mat lbp, Mat roi) {
 	int hcount[256] = { 0 };
@@ -60,6 +66,37 @@ int histogram(Mat lbp, Mat roi) {
 	return 0;
 }
 
+void convert_to_ml( Mat& trainData )
+{
+    //--Convert data
+    const int rows = (int)hogDatas.size();
+    const int cols = (int)std::max( hogDatas[0].cols, hogDatas[0].rows );
+    Mat tmp( 1, cols, CV_32FC1 ); //< used for transposition if needed
+    trainData = Mat( rows, cols, CV_32FC1 );
+
+    for( size_t i = 0 ; i < hogDatas.size(); ++i )
+    {
+        CV_Assert( hogDatas[i].cols == 1 || hogDatas[i].rows == 1 );
+
+        if( hogDatas[i].cols == 1 )
+        {
+            transpose( hogDatas[i], tmp );
+            tmp.copyTo( trainData.row( (int)i ) );
+        }
+        else if( hogDatas[i].rows == 1 )
+        {
+            hogDatas[i].copyTo( trainData.row( (int)i ) );
+        }
+    }
+}
+
+void hogCompute(Mat lbp){
+	vector<float>  hogDescriptors;
+	resize(lbp, lbp, Size(64,64), 0, 0, CV_INTER_AREA);
+	hog->compute(lbp, hogDescriptors,Size(1,1), Size(0,0)); 
+	hogDatas.push_back( Mat( hogDescriptors ).clone() );
+}
+
 int main()
 {
 	Mat lbp, roi;
@@ -70,11 +107,12 @@ int main()
 		roi = imread(loadLocationC + to_string(i) + "_roi" + fileType);
 		//imshow("lbp", lbp);
 		//imshow("roi", roi);
-		histogram(lbp, roi);
-		for (int j = 0; j<256; j++) {
-			histogramCal[i - 1][j] = histTemp[j];
-			cout << histogramCal[i - 1][j] << endl;
-		}
+		//histogram(lbp, roi);
+		hogCompute(lbp);
+		// for (int j = 0; j<256; j++) {
+		// 	histogramCal[i - 1][j] = histTemp[j];
+		// 	cout << histogramCal[i - 1][j] << endl;
+		// }
 	}
 	for (int i = 1; i <= otherAmount; i++) {
 		//file << loadLocationO+to_string(i)+"_lbp"+fileType << "\n";
@@ -83,11 +121,12 @@ int main()
 		roi = imread(loadLocationO + to_string(i) + "_roi" + fileType);
 		//imshow("lbp", lbp);
 		//imshow("roi", roi);
-		histogram(lbp, roi);
-		for (int j = 0; j<256; j++) {
-			histogramCal[i + cloudAmount - 1][j] = histTemp[j];
-			cout << histogramCal[i + cloudAmount - 1][j] << endl;
-		}
+		//histogram(lbp, roi);
+		hogCompute(lbp);
+		// for (int j = 0; j<256; j++) {
+		// 	histogramCal[i + cloudAmount - 1][j] = histTemp[j];
+		// 	cout << histogramCal[i + cloudAmount - 1][j] << endl;
+		// }
 	}
 	for (int i = 0; i<cloudAmount + otherAmount; i++) {
 		if (i<cloudAmount)
@@ -95,13 +134,15 @@ int main()
 		else
 			tag[i] = -1;
 	}
+	Mat hogTrain_data;
+	convert_to_ml( hogTrain_data );
 
 	const int num_data = cloudAmount + otherAmount; //資料數
-	const int num_column = 256; //欄位數
+	//const int num_column = 256; //欄位數
 
-	Mat trainingDataMat(num_data, num_column, CV_32FC1, histogramCal);
+	//Mat trainingDataMat(num_data, num_column, CV_32FC1, histogramCal);
 	Mat labelsMat(num_data, 1, CV_32SC1, tag);
-	Ptr<TrainData> trainingData = TrainData::create(trainingDataMat, ROW_SAMPLE, labelsMat);
+	Ptr<TrainData> trainingData = TrainData::create(hogTrain_data, ROW_SAMPLE, labelsMat);
 	
 	SVM::ParamTypes params;
 	SVM::KernelTypes kernel_type = SVM::LINEAR;
@@ -109,7 +150,7 @@ int main()
 	svm->setKernel(kernel_type);
 
 	svm->trainAuto(trainingData);
-	svm->save(record + "SVM.xml");
+	svm->save(record + "SVM_hog.xml");
 
 	system("pause");
 	return 0;
