8299fcf36a6e126fc1c680639bd78f943b0c5667
chi <ciyalnin@gmail.com>
2018-08-07 18:14:51 +0800

    [clean code]

--- Source.cpp
+++ Source.cpp
@@ -6,64 +6,6 @@
 using namespace cv;
 using namespace std;
 
-Mat LBP(Mat src_image)
-{
-	Mat temp_image = src_image;
-	Mat Image(temp_image.rows, temp_image.cols, CV_8UC1);
-	Mat lbp(temp_image.rows, temp_image.cols, CV_8UC1);
-
-	if (temp_image.channels() == 3)
-		cvtColor(temp_image, Image, CV_BGR2GRAY);
-
-	//imshow("src_image", Image);
-
-	int center = 0;
-	int center_lbp = 0;
-
-	for (int row = 1; row < Image.rows - 1; row++)
-	{
-		for (int col = 1; col < Image.cols - 1; col++)
-		{
-
-			center = Image.at<uchar>(row, col);
-			center_lbp = 0;
-
-			if (center <= Image.at<uchar>(row - 1, col - 1))
-				center_lbp += 1;
-
-			if (center <= Image.at<uchar>(row - 1, col))
-				center_lbp += 2;
-
-			if (center <= Image.at<uchar>(row - 1, col + 1))
-				center_lbp += 4;
-
-			if (center <= Image.at<uchar>(row, col - 1))
-				center_lbp += 8;
-
-			if (center <= Image.at<uchar>(row, col + 1))
-				center_lbp += 16;
-
-			if (center <= Image.at<uchar>(row + 1, col - 1))
-				center_lbp += 32;
-
-			if (center <= Image.at<uchar>(row + 1, col))
-				center_lbp += 64;
-
-			if (center <= Image.at<uchar>(row + 1, col + 1))
-				center_lbp += 128;
-
-			//cout << "center lbp value: " << center_lbp << endl;
-			lbp.at<uchar>(row, col) = center_lbp;
-		}
-	}
-
-	//imshow("lbp_image", lbp);
-	//waitKey(0);
-	//destroyAllWindows();
-
-	return lbp;
-}
-
 int main()
 {
 	Mat image;
@@ -71,11 +13,11 @@ int main()
 	Scalar bgColor = Scalar(255, 0, 255);
 	String filePlace = "E:/testvs/pdata/0713/";
 	String srcfileType = ".jpg";
-	String srcfilePlace = "E:/testvs/pdata/srcImg/img 453-836 (england)/";
-	int fIndex=453;
+	String srcfilePlace = "E:/testvs/pdata/srcImg/";
+	int fIndex = 1;
 	const time_t ctt = time(0);
 	cout << asctime(localtime(&ctt)) << std::endl;
-	while (true)
+	while (fIndex < 3)
 	{
 		String imgName = to_string(fIndex);
 		image = cv::imread(srcfilePlace + imgName + srcfileType);
@@ -89,14 +31,12 @@ int main()
 			return -1;
 		}
 
-		int leftX = image.cols, leftY = image.rows, rightX = 0, rightY = 0;
 		int allArea = image.cols * image.rows;
-		//cout << leftX << " " << leftY << " " << rightX << " " << rightY << "\n";
 
 		//grabCut
 		Mat result(image.size(), CV_8UC1, Scalar(GC_BGD)); // segmentation result (4 possible values) (second)
-		Mat resultTemp;									   // segmentation result (4 possible values) (fist)
-		Mat bgModel, fgModel;							   // the models (internally used)
+		// Mat resultTemp;									   // segmentation result (4 possible values) (fist)
+		Mat bgModel, fgModel; // the models (internally used)
 		Mat foregroundTemp(image.size(), CV_8UC3, bgColor);
 
 		// 前後景MASK
@@ -114,12 +54,11 @@ int main()
 				{
 					ptr2[x] = GC_PR_FGD;
 				}
-
 			}
 		}
 
-		compare(result, GC_PR_FGD, resultTemp, CMP_EQ);
-		image.copyTo(foregroundTemp, resultTemp); // bg pixels not copied
+		// compare(result, GC_PR_FGD, resultTemp, CMP_EQ);
+		// image.copyTo(foregroundTemp, resultTemp); // bg pixels not copied
 
 		grabCut(image,					// input image
 				result,					// segmentation result
@@ -131,13 +70,15 @@ int main()
 
 		compare(result, cv::GC_PR_FGD, result, cv::CMP_EQ);
 		Mat foreground(image.size(), CV_8UC3, bgColor);
+		image.copyTo(foreground, result); //RGB cloud on backgroundColor
+
 		Mat foregroundBinary(image.size(), CV_8UC1, cv::Scalar(0));
 		Mat whiteImg(image.size(), CV_8UC1, cv::Scalar(255));
+		whiteImg.copyTo(foregroundBinary, result); // White cloud on black background
+
 		Mat getContours(image.size(), CV_8UC3, cv::Scalar(0, 0, 0));
 		Mat getHulls_b(image.size(), CV_8UC1, cv::Scalar(0));
-		Mat imgLBP;
-		image.copyTo(foreground, result); // bg pixels not copied
-		whiteImg.copyTo(foregroundBinary, result);
+		//Mat imgLBP;
 
 		vector<vector<Point>> contours;
 		vector<Vec4i> hierarchy;
@@ -148,25 +89,14 @@ int main()
 			double subArea = contourArea(contours[i], false);
 			if (subArea / allArea > 0.0005)
 			{
-				int contoursIndex = 0;
-				Rect bounding_rect = boundingRect(contours[i]);
-				Mat foregroundROI = foreground(bounding_rect);
-				imgLBP = LBP(foregroundROI);
-				drawContours(getContours, contours, i, bgColor, CV_FILLED, 8, hierarchy);
-				cv::rectangle(getContours, bounding_rect, cv::Scalar(0, 0, 255), 2);
-
 				convexHull(Mat(contours[i]), hull[i], false);
 				drawContours(getHulls_b, hull, i, Scalar(255), CV_FILLED, 8, hierarchy);
-				drawContours(image, contours, i, cv::Scalar(0, 0, 255), 2, 8, hierarchy);
-				imwrite(filePlace+"/lbp/" + imgName +"_" +to_string(i)+"_lbp.jpg", imgLBP);
-				imwrite(filePlace + "/lbp/" + imgName + "_" + to_string(i) + "_roi.jpg", foregroundROI);
 			}
 		}
-
-		imwrite(filePlace+"/img/" + imgName + "_a.jpg", image);
-		imwrite(filePlace+"/img/" + imgName + "_b.jpg", foregroundBinary);
-		imwrite(filePlace+"/img/" + imgName + "_c.jpg", getContours);
-		imwrite(filePlace+"/hull/" + imgName + ".jpg", getHulls_b);
+		namedWindow("image" + to_string(fIndex));
+		imshow("image" + to_string(fIndex), image);
+		namedWindow("hull" + to_string(fIndex));
+		imshow("hull" + to_string(fIndex), getHulls_b);
 
 		fIndex++;
 	}
